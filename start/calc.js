cf35c9e51771f14ccbd0137e188fb602d7ea8a9b
praveenchakravarthy <praveenchakravarthy21@gmail.com>
2020-05-08 20:48:40 +0530

    using full concept of todays class

---
+++ start/calc.js
@@ -0,0 +1,32 @@
+let add = (a,b) =>
+{
+    let c=a+b
+    return c
+}
+let addition_result = add(2,3)
+console.log(`Addition : ${addition_result}`)
+
+function sub (a,b)
+{
+    let c=a-b
+    return c
+}
+
+let sub_result = sub(4,3)
+console.log(`subtraction : `+sub_result)
+
+let multiply =(a,b) =>
+{
+    let c=a*b
+    return c
+}
+
+let multiply_result =multiply(5,5)
+console.log(`multiplication : ${multiply_result}`)
+
+function div (a,b)
+{
+let divide_result = a/b;
+console.log(`division :${divide_result}`)
+}
+div (6,2)
