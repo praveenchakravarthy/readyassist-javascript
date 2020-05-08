52029ca17cc0057a7ce01d8813140530d398da00
praveenchakravarthy <praveenchakravarthy21@gmail.com>
2020-05-08 20:16:23 +0530

    arthmatic calculation

---
+++ start/calculator.js
@@ -0,0 +1,35 @@
+let add = (a,b) =>
+{
+    let c=a+b
+    return c
+}
+let addition_result = add(2,3)
+console.log(`Addition : ${addition_result}`)
+
+let sub = (a,b) =>
+{
+    let c=a-b
+    return c
+}
+
+let sub_result = sub(4,3)
+console.log(`subtraction : `+sub_result)
+
+let multiply =(a,b) =>
+{
+    let c=a*b
+    return c
+}
+
+let multiply_result =multiply(5,5)
+console.log(`multiplication : ${multiply_result}`)
+
+let div = (a,b) =>
+{
+    let c=a/b
+    return c
+}
+let divide_result =div(6,3)
+console.log(`division :`, divide_result)
+
+
