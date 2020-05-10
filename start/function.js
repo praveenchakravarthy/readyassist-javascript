b3eea4a6f182907a3856860375e9f7925fdc0d62
praveenchakravarthy <praveenchakravarthy21@gmail.com>
2020-05-10 09:47:30 +0530

    call back function

---
+++ start/function.js
@@ -0,0 +1,17 @@
+function print()
+{
+    console.log('Iam Praveen Chakravarthy')
+    
+}
+
+function name(c)
+{
+    console.log('Hello World!')
+    if (c){
+        c();
+     }
+     
+}
+
+name(print)
+
