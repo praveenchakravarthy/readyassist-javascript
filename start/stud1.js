3ec7bdf3e71ec16b1ae23f6fbab567ae35174610
praveenchakravarthy <praveenchakravarthy21@gmail.com>
2020-05-11 14:01:24 +0530

    another test

---
+++ start/stud1.js
@@ -0,0 +1,11 @@
+let name = ['pc','cc','ak','mm','ks']
+name.forEach(element => {
+    console.log(element)
+    
+});
+console.log(name)
+console.log(name.length)
+{
+    for (i=0;i<5;i++)
+    console.log(name[i])
+}
