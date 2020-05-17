c969e05f36029b65a0cb61d0326ed5587e3f4885
praveenchakravarthy <praveenchakravarthy21@gmail.com>
2020-05-17 15:22:54 +0530

    using var

---
+++ start/calcus.js
@@ -0,0 +1,35 @@
+var add = (a,b) =>
+{
+    var c=a+b
+    return c
+}
+var addition_result = add(2,3)
+console.log(`Addition : ${addition_result}`)
+
+var sub = (a,b) =>
+{
+    var c=a-b
+    return c
+}
+
+var sub_result = sub(4,3)
+console.log(`subtraction : `+sub_result)
+
+var multiply =(a,b) =>
+{
+    var c=a*b
+    return c
+}
+
+var multiply_result =multiply(5,5)
+console.log(`multiplication : ${multiply_result}`)
+
+var div = (a,b) =>
+{
+    var c=a/b
+    return c
+}
+var divide_result =div(6,3)
+console.log(`division :`, divide_result)
+
+
